338d7845b299f003b56924254a8f9fde55f60b88
amir247 <amir_hamza@outlook.com>
2026-04-21 21:14:59 +0200

    did init on both frontend beacuse it has backend inside

---
+++ backend/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.SignalR;
+
+public class ChatHub : Hub
+{
+  public async Task JoinChat(UserConnection conn)
+  {
+    await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined");
+  }
+
+
+  public async Task JoinSpecificChatRoom(UserConnection conn)
+  {
+    await Groups.AddToGroupAsync(Context.ConnectionId, conn.Chatroom);
+    await Clients.Group(conn.Chatroom).SendAsync("ReceiveMessage", "admin", $"{conn.Chatroom} has joined {conn.Chatroom}");
+  }
+
+
+
+}
---
+++ backend/Models/UserConnection.cs
@@ -0,0 +1,6 @@
+public class UserConnection
+{
+  public string Username {get; set;} = string.Empty;
+  public string Chatroom {get; set;} = string.Empty;
+
+}
